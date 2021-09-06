5e70bab05c0cacadf8e1021c6f94bc5a9fb5ceee
Tansu03 <75196663+Tansu03@users.noreply.github.com>
2021-09-06 15:03:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 img="";
 status="";
-object=[];
+objects=[];
 function setup()
 {
     canvas=createCanvas(380,380);
@@ -15,7 +15,7 @@ function setup()
 function modelLoaded()
 {
   console.log("Model Loaded");
-  status="true";
+  status= true;
 }
 
 function gotResults(error, results)
@@ -25,7 +25,7 @@ function gotResults(error, results)
      console.log(error);
    }
    console.log(results);
-   object=results;
+   objects=results;
 }
 
 function preload()
@@ -34,22 +34,22 @@ function preload()
 function draw()
 {
     image(video,0,0,380,380);
-    if (status=="true")
+    if (status== true)
     {
       r= random(225);
       g= random(225);
       b= random(225);
       objectDetector.detect(video, gotResults);
-       for(i=0; i<object.length; i++) 
+       for(i=0; i<objects.length; i++)
        {
        document.getElementById("status").innerHTML= "Status= Object Detected";
-       document.getElementById("number_of_objects").innerHTML= "Number of objects="+object.length;
+       document.getElementById("number_of_objects").innerHTML= "Number of objects="+objects.length;
        fill(r,g,b);
-       percent=floor(object[i].confidence*100);
-       text(object[i].label+" "+ percent+ "%", object[i].x+20, object[i].y+20);
+       percent=floor(objects[i].confidence*100);
+       text(objects[i].label+" "+ percent+ "%", objects[i].x+20, objects[i].y+20);
        noFill();
        stroke(r,g,b);
-       rect(object[i].x, object[i].y, object[i].width, object[i].height);
+       rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
        }
     }
 }
